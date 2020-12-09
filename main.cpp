6cebe6ab4f676cd5e4cf2d2e51de3a3b86841c4f
cho102 <cho102@ucr.edu>
2020-12-08 18:27:52 -0800

    simplified main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "UserGuide.hpp"
 #include "Task.hpp"
 #include "Category.hpp"
+#include "CompositeInput.hpp"
 #include "ConcreteFactory.hpp"
 #include "PlaylistSubclasses.hpp"
 
@@ -9,17 +10,6 @@
 #include <string>
 
 using namespace std;
-
-void printMenu();
-
-
-/*Composite Pattern Functions*/
-void AddToGuide();
-void PrintTop();
-void EnterNewGuide();
-Category* AccessGuide();
-void addItem(Category*);
-vector<Category*> top;
 
 //Abstract Factory class functions
 void printMenu2();
@@ -29,30 +19,9 @@ void printMenu2();
 
 
 int main() {
-    // composite pattern
-    int control = 1;
-    int input;
-    while (control) {
-      printMenu();
-      cin >> input;
-      if (input == 1) {
-        EnterNewGuide();
-      }
-      else if (input == 2) {
-        AddToGuide();
-      }
-      else if (input == 3) {
-        PrintTop();
-      }
-      else if (input == 0) {
-        cout << "Thank you!" << endl;
-        control = 0;
-      }
-      else {
-        cout << "ENTER VALID NUMBER\n" << endl;
-      }
-    }
-
+	
+	CompositeInput();
+	
   //abstract factory pattern
   int control2 = 1;
   char input2;
@@ -60,10 +29,10 @@ int main() {
   vector<string> chosenartists;
   vector<double> chosentimes;
 
-  while (control) {
+  while (control2) {
     printMenu2();
     cin >> input2;
-    if (input == 'w'){
+    if (input2 == 'w'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the Hip Hop playlist: " << endl;
@@ -72,9 +41,9 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0;
+      control2 = 0;
     }
-    else if (input == 'e'){
+    else if (input2 == 'e'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the EDM playlist: " << endl;
@@ -83,9 +52,9 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0; 
+      control2 = 0; 
     }
-    else if (input == 'p'){
+    else if (input2 == 'p'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the Pop playlist: " << endl;
@@ -94,9 +63,9 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0; 
+      control2 = 0; 
     }
-    else if (input == 'l'){
+    else if (input2 == 'l'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the Lo-Fi playlist: " << endl;
@@ -105,9 +74,9 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0; 
+      control2 = 0; 
     }
-    else if (input == 'r'){
+    else if (input2 == 'r'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the RnB playlist: " << endl;
@@ -116,9 +85,9 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0; 
+      control2 = 0; 
     }
-    else if (input == 'n'){
+    else if (input2 == 'n'){
       ConcreteFactory* choice = new ConcreteFactory();
       string pname;
       cout << "Enter a name for the Nature Sounds playlist: " << endl;
@@ -127,7 +96,7 @@ int main() {
       chosensongs = user->getSongs();
       chosenartists = user->getArtists();
       chosentimes = user->getTimes();
-      control = 0; 
+      control2 = 0; 
     }
     else {
       cout << "Not a valid input. Please try again." << endl;
@@ -140,112 +109,6 @@ int main() {
 //ruth when you start the vectors for u to sort to output will be: chosensongs, chosenartists, chosentimes
 
     return 0;
-}
-
-void printMenu() {
-      cout << "Enter 1 to create new guide." << endl;
-      cout << "Enter 2 add new task/catergory to existing guide." << endl;
-      cout << "Enter 3 to print out guide." << endl;
-      cout << "Enter 0 to quit program." << endl;
-      cout << "Enter job you want to do: ";
-}
-
-
-void EnterNewGuide() {
-  string name;
-  cout << "Enter name of new guide planner: ";
-  cin >> name;
-  cout << "Enter hours planned to study each day: ";
-  double hpd;
-  cin >> hpd;
-  cout << "Enter how many days until D-Day: ";
-  int dday;
-  cin >> dday;
-  cout << endl;
-  Category* main = new Category (name, hpd, dday);
-  top.push_back(main);
-  addItem(main);
-}
-
-Category* AccessGuide() {
-  cout << "List of Guides: " << endl;
-  for (int i = 0; i < top.size(); ++i) {
-    cout << i+1 << ". " << top.at(i)->getName() << endl;
-  }
-  cout << endl;
-  int test = 1;
-  int num;
-  while (test) {
-      cout << "Enter the corresponding number of guide you want to access: " << endl;
-      cin >> num;
-      if (!(num < 1) || !(num >= top.size())) {
-        test = 0;
-      }
-      else {
-        cout << "Error!" << endl;
-      }
-  }
-  return top.at(num-1);
-}
-
-void addItem(Category* temp) {
-  int ctrl = 1;
-  int input;
-  while (ctrl) {
-    cout << "Enter 1 for new Category in " << temp->getName() << ".\n";
-    cout << "Enter 2 for new Task in " << temp->getName() << ".\nEnter 3 to quit \n";
-    cin >> input;
-    if (input == 1) {
-      string name1;
-      cout << "Enter name of Category: ";
-      cin >> name1;
-      Category* subcat = new Category(name1, 0, 0);
-      temp->add(subcat);
-      top.push_back(subcat);
-      cout << endl;
-    }
-    else if (input == 2) {
-      string name2;
-      cout << "Enter name of Task: ";
-      cin >> name2;
-      Task* task = new Task(name2);
-      temp->add(task);
-      cout << endl;
-    }
-    else if (input == 3) {
-      cout << endl;
-      ctrl = 0;
-    }
-    else {
-      cout << "Enter valid number!" << endl;
-    }
-
-  }
-}
-
-void AddToGuide() {
-        if (top.size() == 0) {
-          cout << "No guides to access.\n" << endl;
-        }
-        else {
-          Category* temp = AccessGuide();
-          addItem(temp);
-        }
-}
-
-void PrintTop() {
-        if (top.size() == 0) {
-          cout << "No guides to access.\n" << endl;
-        }
-        else {
-          for (int i = 0; i < top.size(); ++i) {
-            top.at(i)->calculateTimeEachDay();
-          }
-          Category* print = AccessGuide();
-          cout << "Guide (Total days: " << print->getMax() << "): " << endl;
-          print->printGuide();
-          cout << endl;
-        }
 }
 
 void printMenu2() {
