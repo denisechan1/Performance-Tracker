7aa181246db2abe2e8cc5baa4cd50bcd4cb961c1
denisechan1 <chanxdenise@gmail.com>
2020-12-10 00:35:50 -0800

    Added basic strategy tests pt 1

--- strategyTests.hpp
+++ strategyTests.hpp
@@ -10,20 +10,39 @@
 #include "songAlphabetical.hpp"
 #include "songLength.hpp"
 
-// TEST(songLength, lengthTest1){
-//     string x = "playlist";
-//     HipHopPlaylist* test = new HipHopPlaylist(x);
+TEST(songLength, HHsongTest1){
+     string x = "playlist";
+     HipHopPlaylist* test = new HipHopPlaylist(x);
 
-//     artistAlphabetical* test1 = new artistAlphabetical(test);
+     artistAlphabetical* test1 = new artistAlphabetical(test);
+     test1->insertionSort();
+     vector<string> v = test1->getS();
+     string result = v.at(0);
 
-//     test1->insertionSort();
+    EXPECT_EQ(result, "Grey Area");  //after alphabetical i think 2 Chainz should be first artist so Grey Area first?
+}
+TEST(songLength, HHartistTest1){
+     string x = "playlist";
+     HipHopPlaylist* test = new HipHopPlaylist(x);
 
-//     string result = 
-    
+     artistAlphabetical* test1 = new artistAlphabetical(test);
+     test1->insertionSort();
+     vector<string> v = test1->getA();
+     string result = v.at(0);
 
-    //EXPECT_EQ();
-//}
+    EXPECT_EQ(result, "2 Chainz");  //after alphabetical i think 2 Chainz should be first artist?
+}
+TEST(songLength, HHlengthTest1){
+     string x = "playlist";
+     HipHopPlaylist* test = new HipHopPlaylist(x);
 
+     artistAlphabetical* test1 = new artistAlphabetical(test);
+     test1->insertionSort();
+     vector<double> v = test1->getL();
+     double result = v.at(0);
+
+    EXPECT_EQ(result, 2.520000);  //after alphabetical i think 2 Chainz should be first artist?
+}
     
 
 #endif //__STRATEGY_TESTS_HPP
